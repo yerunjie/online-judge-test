3b08ae3d7963eac7c6d8cb8cd333b06f6dc3ea88
yerunjie <roger.ye1996@gmail.com>
2019-04-01 23:45:34 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(s);
+        System.out.println("yerunjie123");
+    }
+}
+
